c04f7c33e87d5a88c83a3d6fcdcb26ac2f6080f5
ShutongLin <71856038+ShutongLin@users.noreply.github.com>
2020-09-28 09:50:30 -0500

    Update CourseReading.java

--- CourseReading.java
+++ CourseReading.java
@@ -6,3 +6,49 @@
 // TA: <Brianna (Bri) Cochran>
 // Lecturer: <Gary Dahl>
 // Notes to Grader: <optional extra notes>
+//-------------------------------------------------------------------------Not finish yet-----------------------------------------------------------------------------------------
+class CourseReading {
+	private int isbn;
+	private String bookTitle;
+	private String course;
+
+	public CourseReading(int isbn, String bookTitle, String course) {
+		super();
+		this.isbn = isbn;
+		this.bookTitle = bookTitle;
+		this.course = course;
+	}
+
+	public CourseReading(String[] csvLineContent) {
+		this(Integer.parseInt(csvLineContent[0]), csvLineContent[1], csvLineContent[2]);
+	}
+
+	public int getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(int isbn) {
+		this.isbn = isbn;
+	}
+
+	public String getBookTitle() {
+		return bookTitle;
+	}
+
+	public void setBookTitle(String bookTitle) {
+		this.bookTitle = bookTitle;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	public String toCSVContent() {
+		return String.join(",", String.valueOf(this.isbn), this.bookTitle, this.course);
+	}
+
+}
